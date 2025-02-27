ad0a09f1334970ecec041501d7078619361db21d
nguyenle <nguyentaro1502@gmail.com>
2025-02-27 17:37:28 +0700

    add reload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,12 @@ const Header = ({ onSearch }) => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 bg-black p-4 flex items-center justify-between">
       <div className="flex items-center space-x-6">
-        <h1 className="text-[30px] uppercase font-bold text-red-700">MOVIE</h1>
+        <h1
+          className="text-[30px] uppercase font-bold text-red-700 cursor-pointer"
+          onClick={() => window.location.reload()}
+        >
+          MOVIE
+        </h1>
         <nav className="hidden md:flex items-center space-x-4">
           <a href="" className="text-white">
             Phim Hot
